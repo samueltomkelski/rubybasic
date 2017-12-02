7782cf5147ba4376af5d01c38d179029d5cd4036
Samuel Tomkelski <samueltomkelski@hotmail.com>
2017-12-02 10:46:55 -0200

    fuck

--- curso1.rb
+++ curso1.rb
@@ -1,16 +1,16 @@
-    var1 = 100
-    var2 = true
-    var3 = "teste2234"
+var1 = 100
+var2 = true
+var3 = "CURSO AUTOMAÇÃO 2.0 0 NOVO BRANCH ALTERADO xxxx dnfsjknfkjsdnfsdfsd"
 
-    puts var1
-    puts var2
-    puts var3
+puts var1
+puts var2
+puts var3
 
-    puts "teste: #{var1}"
+puts "teste: #{var1}"
 
-    cont = 0
-    loop do
-        cont += 1
-        puts cont
-        break if cont == var1
-    end
+cont = 0
+loop do
+    cont += 1
+    puts cont
+    break if cont == var1
+end
